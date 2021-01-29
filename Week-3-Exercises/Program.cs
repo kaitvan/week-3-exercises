95bfe2e7a6995c6fa71c0799f973f27912b39edb
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2021-01-28 21:30:01 -0600

    heist exercise completed

---
+++ Week-3-Exercises/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Week_3_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine();
+            Console.Write("Enter the difficulty level of the bank as as number between 0 and 100: ");
+            int bankDifficulty = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            var team = new Team();
+
+            var name = "";
+            double teamSkillLevel = 0;
+
+            do
+            {
+                Console.Write("Enter a team member's name: ");
+                name = Console.ReadLine();
+                if (name == "")
+                {
+                    break;
+                }
+                else
+                {
+                    var teamMember = new TeamMember(name);
+                    teamMember.Create();
+                    teamSkillLevel += teamMember.SkillLevel;
+
+                    TeamMember.TeamMemberCount++;
+                    team.AddTeamMember(teamMember);
+                }
+            }
+            while (name != "");
+
+            Console.WriteLine();
+            Console.WriteLine($"You have {TeamMember.TeamMemberCount} team members for this heist.");
+            Console.WriteLine();
+            Console.Write("How many trial runs do you want to perform? ");
+            int trialRuns = Convert.ToInt32(Console.ReadLine());
+            int successfulTrials = 0;
+            int failedTrials = 0;
+
+            for (var i = 1; i <= trialRuns; i++)
+            {
+                Random randomNumber = new Random();
+                int luckValue = randomNumber.Next(-10, 10);
+                int difficultyAndLuck = bankDifficulty + luckValue;
+
+
+                Console.WriteLine($"The team's combined skill level is {teamSkillLevel}.");
+                Console.WriteLine($"The bank's difficulty level is {difficultyAndLuck}.");
+
+                if (teamSkillLevel >= difficultyAndLuck)
+                {
+                    Console.WriteLine("Your heist was a success!");
+                    successfulTrials++;
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("You fail.");
+                    failedTrials++;
+                    Console.WriteLine();
+                }
+            }
+
+            double successRate = Math.Round(Convert.ToDouble(successfulTrials) / Convert.ToDouble(trialRuns) * 100);
+            Console.WriteLine($"You succeeded {successfulTrials} out of {trialRuns} times. That's a {successRate}% success rate.");
+
+
+        }
+    }
+}
---
+++ Week-3-Exercises/TeamMember.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week_3_Exercises
+{
+    class TeamMember
+    {
+        public string Name { get; set; }
+        public double SkillLevel { get; set; }
+        public double CourageFactor { get; set; }
+        public static int TeamMemberCount = 0;
+
+        public TeamMember(string name)
+        {
+            Name = name;
+        }
+
+        public void Create()
+        {
+            Console.Write("Enter this team member's skill level: ");
+            SkillLevel = Math.Round(Convert.ToDouble(Console.ReadLine()));
+            Console.Write("Enter this team member's courage factor: ");
+            CourageFactor = Convert.ToDouble(Console.ReadLine());
+
+            if (CourageFactor < 0)
+            {
+                CourageFactor = 0;
+            }
+            else if (CourageFactor > 2)
+            {
+                CourageFactor = 2;
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("***Team Member Information***");
+            Console.WriteLine($"Team Member: {Name}");
+            Console.WriteLine($"Skill Level: {SkillLevel}");
+            Console.WriteLine($"Courage Factor: {CourageFactor}");
+        }
+    }
+}
